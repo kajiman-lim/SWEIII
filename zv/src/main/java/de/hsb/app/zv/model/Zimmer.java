383439fcd7aa507c6e6db57aabe7cdce173ea6cb
kajiman-lim <38255131+kajiman-lim@users.noreply.github.com>
2021-03-27 17:51:52 +0100

    .

--- zv/src/main/java/de/hsb/app/zv/model/Zimmer.java
+++ zv/src/main/java/de/hsb/app/zv/model/Zimmer.java
@@ -14,7 +14,6 @@ import javax.persistence.NamedQuery;
 @NamedQuery(name="SelectZimmer", query="Select z from Zimmer z")
 //@NamedQuery(name = "SearchZimmer", query = "SELECT z FROM Zimmer z LEFT OUTER JOIN Reservierung r ON z.id=r.zimmer WHERE ((r.von not between ?1 and ?2) or r.von = null) AND ((r.bis not between ?1 and ?2) or r.bis = null)")
 @NamedQuery(name = "SearchZimmer", query = "SELECT z FROM Zimmer z WHERE NOT EXISTS(SELECT r FROM Reservierung r WHERE r.zimmer=z.id AND ((r.von BETWEEN ?1 AND ?2) OR (r.bis BETWEEN ?1 AND ?2)))")
-
 @NamedQuery(name="SelectNull", query="Select z FROM Zimmer z WHERE 0 != 0")
 @Entity
 @RequestScoped
